538e8762b18f9731ef5fd3b6be2d375df20ef71f
Saransh Dwivedi <dwivedisaransh1992@gmail.com>
2023-11-06 20:41:32 +0530

    add polyfills to tsconfig

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -96,13 +96,13 @@ import 'regenerator-runtime/runtime';
 import 'promise-polyfill/src/polyfill';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js'; // Run `npm install --save classlist.js`.
+//import 'classlist.js'; // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
-import 'web-animations-js'; // Run `npm install --save web-animations-js`.
+//import 'web-animations-js'; // Run `npm install --save web-animations-js`.
 
 /** Evergreen browsers require these. **/
-import 'core-js/es/reflect';
+//import 'core-js/es/reflect';
 
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
@@ -117,6 +117,6 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-import 'intl'; // Run `npm install --save intl`.
+//import 'intl'; // Run `npm install --save intl`.
 
-import 'whatwg-fetch';  //Run npm install whatwg-fetch --save; 
+//import 'whatwg-fetch';  //Run npm install whatwg-fetch --save; 
