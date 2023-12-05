5466001f01872babea482b2c9e46f2e2bcb443a0
Saransh Dwivedi <dwivedisaransh1992@gmail.com>
2023-12-05 19:48:13 +0530

    remove app and do fresh installation

--- index-html-transform.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (targetOptions, indexHtmlContent) => {
-  if (typeof indexHtmlContent === 'string') {
-    indexHtmlContent = indexHtmlContent.replaceAll("type=\"module\"",
-      "");
-  }
-
-  console.log("*****************");
-  return indexHtmlContent;
-}
